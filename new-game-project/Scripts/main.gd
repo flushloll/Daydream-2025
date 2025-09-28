97940c6af232f23921efb2c93ae226afa2fe4765
StillPillWill <wetwilman20@gmail.com>
2025-09-28 13:50:51 -0700

    :(

--- new-game-project/Scripts/main.gd
+++ new-game-project/Scripts/main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var pNode
+var nodes=[]
 
 func _ready():
 	pNode=preload("res://person_node.tscn")
@@ -13,6 +14,19 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_down"):
 		var c=pNode.instantiate()
 		c.scale*=10
-		c.position=get_global_mouse_position()
+		c.position=Vector2.ZERO
+		c.position.x=int(get_global_mouse_position().x)%20
+		c.position.y=int(get_global_mouse_position().y)%20
+		c.position*20
 		add_child(c)
-	
+		nodes.append(c)
+		
+func _physics_process(delta: float) -> void:
+	for x in nodes:
+		for y in nodes:
+			if x==y:
+				return
+			if abs(x.global_position-y.global_position).x<100:
+				print(x.global_position-y.global_position)
+				x.position.x+=50
+				x.position.y+=50
--- new-game-project/Scripts/person_node.gd
+++ new-game-project/Scripts/person_node.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var namae
 var attribute={
 	"Charisma": 1,
 	"Optimism":1,
@@ -7,14 +7,21 @@ var attribute={
 	"Health":1,
 	"Ambition":1
 }
+
+var inRange=[]
+
 var pastMousePos=Vector2.ZERO 
 var initPos=Vector2.ZERO
-
+var camInitPos=Vector2.ZERO
 @onready var camera=get_parent().get_node("Camera2D")
 var midDown=false
 var mouseIn=false
 var mouseDown=false
 var mousePos
+
+var grid=[-2,-1,0,1,2]
+var taken=[false,false,false,false]
+
 func _ready():
 	pass
 
@@ -28,18 +35,24 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("midDown"):
 		midDown=true
 		initPos=get_viewport().get_mouse_position()
-		
+		camInitPos=camera.position
 	if Input.is_action_just_released("midDown"):
 		midDown=false
 		initPos=Vector2.ZERO
 	if midDown:
-		camera.position=-1*(1/camera.scale.x)*(get_viewport().get_mouse_position()-initPos)
+		camera.position=camInitPos-1*pow(1.2/camera.scale.x,3)*(get_viewport().get_mouse_position()-initPos)
+		#print(initPos)
 	else:
 		initPos=get_viewport().get_mouse_position()	
 	if mouseDown and mouseIn:
 		global_position=mousePos
-		
-
+	if not mouseDown and int(position.y)%20!=0:
+		position.y=(int(position.y)%20)*20
+		print(int(position.y))
+	if not mouseDown and int(position.x)%20!=0:
+		position.x=(int(position.x)%20)*20
+		print(int(position.x))
+			
 func _on_area_2d_mouse_entered() -> void:
 	mouseIn=true
 
@@ -47,3 +60,11 @@ func _on_area_2d_mouse_exited() -> void:
 	if mouseDown:
 		return
 	mouseIn=false
+
+
+func _on_effect_area_entered(area: Area2D) -> void:
+	inRange.append(area)
+	#print(inRange)
+
+func _on_effect_area_exited(area: Area2D) -> void:
+	inRange.remove_at(inRange.find(area))
